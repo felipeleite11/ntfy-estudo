ba2bc2d1137f7eb0a8eed6c6f30a4bed69a62a99
Felipe Leite <lipi4shared@hotmail.com>
2026-04-11 16:43:41 -0300

    aj

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import { useEffect } from 'react'
 import './App.css'
 
-const topic = 'Teste'
+const topic = 'teste'
 const ntfyUrl = `https://outros-ntfy.y0nyoi.easypanel.host/${topic}/sse`
+
+// const publishURL = `https://ntfy.sh/example`;
+// const subscribeURL = `https://ntfy.sh/example/sse`;
+// const events = document.getElementById('events');
+// const eventSource = new EventSource(subscribeURL);
 
 interface NtfyMessage {
 	event: string
@@ -11,19 +16,6 @@ interface NtfyMessage {
 }
 
 function App() {
-	function exibirNotificacaoNaTela(data: NtfyMessage) {
-		console.log('NOTIFICAÇÃO', data)
-
-		// if (Notification.permission === "granted") {
-		// 	new Notification(data.title || "Nova Mensagem", {
-		// 		body: data.message,
-		// 		icon: 'https://ntfy.sh/static/img/ntfy.png'
-		// 	});
-		// } else if (Notification.permission !== "denied") {
-		// 	Notification.requestPermission();
-		// }
-	}
-
 	useEffect(() => {
 		const eventSource = new EventSource(ntfyUrl)
 
@@ -31,14 +23,24 @@ function App() {
 			const data = JSON.parse(event.data) as NtfyMessage
 
 			if (data.event === 'message') {
-				console.log('Nova notificação recebida:', data.message);
-				exibirNotificacaoNaTela(data)
+				console.log('Nova notificação recebida:', data.message)
 			}
-		};
+		}
 
 		eventSource.onerror = (error) => {
-			console.error('Erro na conexão com ntfy:', error);
-		};
+			console.error('Erro na conexão com ntfy:', error)
+		}
+
+		// eventSource.onerror = (e) => {
+		// 	let event = document.createElement('div');
+		// 	event.innerHTML = `EventSource error: Failed to connect to ${subscribeURL}`;
+		// 	events.appendChild(event);
+		// };
+		// eventSource.onmessage = (e) => {
+		// 	let event = document.createElement('div');
+		// 	event.innerHTML = e.data;
+		// 	events.appendChild(event);
+		// };
 	}, [])
 
 	return (
