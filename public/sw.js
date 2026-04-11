11d1a562af36c68a8c58c0936cd2b1e51ea0465e
Felipe Leite <lipi4shared@hotmail.com>
2026-04-11 18:40:39 -0300

    Aj

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,3 @@
-self.addEventListener('install', (event) => {
-    console.log('⏳ Service Worker: Instalando...')
-})
-
-self.addEventListener('activate', (event) => {
-    console.log('✅ Service Worker: Ativo e pronto para controlar a página!')
-})
-
 self.addEventListener('notificationclick', function (event) {
     event.notification.close()
 
