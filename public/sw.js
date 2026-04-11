bbe578a6e2c403a0fb0a5d4f628758f123d3b57f
Felipe Leite <lipi4shared@hotmail.com>
2026-04-11 18:35:36 -0300

    Aju

--- public/sw.js
+++ public/sw.js
@@ -10,6 +10,6 @@ self.addEventListener('notificationclick', function (event) {
     event.notification.close()
 
     event.waitUntil(
-        clients.openWindow('/')
+        clients.openWindow('https://vite.dev')
     )
 })
